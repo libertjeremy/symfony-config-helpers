7657da384bfa08f1a7ec536c54fa3e9f6e7a4e26
Jeremy <jeremy.libert@luxapps.lu>
2025-04-01 16:43:58 +0200

    Add AbstractBundle

---
+++ src/Bundle/AbstractBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LibertJeremy\Symfony\ConfigHelpers\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle as BaseAbstractBundle;
+
+abstract class AbstractBundle extends BaseAbstractBundle
+{
+}
