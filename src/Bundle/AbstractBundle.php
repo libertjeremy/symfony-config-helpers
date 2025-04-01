15c3165dd35a2157284deb49e27fa08654be8143
Jeremy <jeremy.libert@luxapps.lu>
2025-04-01 16:49:29 +0200

    Load extension for parameters

--- src/Bundle/AbstractBundle.php
+++ src/Bundle/AbstractBundle.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace LibertJeremy\Symfony\ConfigHelpers\Bundle;
 
+use LibertJeremy\Symfony\ConfigHelpers\Traits\ParametersInitializerHelperTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle as BaseAbstractBundle;
 
 abstract class AbstractBundle extends BaseAbstractBundle
 {
+    use ParametersInitializerHelperTrait;
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        if (\count($config) > 0) {
+            $this->initConfigurationParameters($builder, $config);
+        }
+    }
 }
